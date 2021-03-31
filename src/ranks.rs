4a3733c5ce19ca52966c55d582d00f5c0f30d7a1
Richard Harris <richardharris916@gmail.com>
2021-03-31 06:59:29 -0500

    Implemented Display and `to_string` methods for ranks/suits

--- src/ranks.rs
+++ src/ranks.rs
@@ -1,4 +1,7 @@
+use std::fmt::{Display, Error, Formatter};
+
 /// All possible ranks for cards
+#[derive(Debug)]
 pub enum Rank {
   TWO,
   THREE,
@@ -14,4 +17,51 @@ pub enum Rank {
   KING,
   ACE,
   JOKER,
+}
+
+impl Display for Rank {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(
+      f,
+      "{}",
+      match &self {
+        Rank::TWO => "2",
+        Rank::THREE => "3",
+        Rank::FOUR => "4",
+        Rank::FIVE => "5",
+        Rank::SIX => "6",
+        Rank::SEVEN => "7",
+        Rank::EIGHT => "8",
+        Rank::NINE => "9",
+        Rank::TEN => "10",
+        Rank::JACK => "J",
+        Rank::QUEEN => "Q",
+        Rank::KING => "K",
+        Rank::ACE => "A",
+        Rank::JOKER => "JK",
+      }
+    )
+  }
+}
+
+impl Rank {
+  /// Translates a rank into a humanized string
+  pub fn to_string(&self) -> &str {
+    match &self {
+      Rank::TWO => "Two",
+      Rank::THREE => "Three",
+      Rank::FOUR => "Four",
+      Rank::FIVE => "Five",
+      Rank::SIX => "Six",
+      Rank::SEVEN => "Seven",
+      Rank::EIGHT => "Eight",
+      Rank::NINE => "Nine",
+      Rank::TEN => "Ten",
+      Rank::JACK => "Jack",
+      Rank::QUEEN => "Queen",
+      Rank::KING => "King",
+      Rank::ACE => "Ace",
+      Rank::JOKER => "Joker",
+    }
+  }
 }
--- src/suits.rs
+++ src/suits.rs
@@ -1,8 +1,40 @@
+use std::fmt::{Display, Error, Formatter};
+
 /// All possible suits for cards
+#[derive(Debug)]
 pub enum Suit {
   CLUBS,
   SPADES,
   HEARTS,
   DIAMONDS,
   JOKER,
+}
+
+impl Display for Suit {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(
+      f,
+      "{}",
+      match &self {
+        Suit::CLUBS => "C",
+        Suit::SPADES => "S",
+        Suit::HEARTS => "H",
+        Suit::DIAMONDS => "D",
+        _ => "", // Suit::JOKER => "J",
+      }
+    )
+  }
+}
+
+impl Suit {
+  /// Translates a suit into a humanized string
+  pub fn to_string(&self) -> &str {
+    match &self {
+      Suit::CLUBS => "Clubs",
+      Suit::SPADES => "Spades",
+      Suit::HEARTS => "Hearts",
+      Suit::DIAMONDS => "Diamonds",
+      Suit::JOKER => "Joker",
+    }
+  }
 }
