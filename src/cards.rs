d67cda0a0d8d293e9ce1be1c5b1aa13b24410443
Richard Harris <richardharris916@gmail.com>
2021-03-31 22:39:15 -0500

    Finished Card::from and began tests Removed lib.rs tests module

--- src/cards.rs
+++ src/cards.rs
@@ -1,4 +1,4 @@
-use std::fmt::{Display, Formatter, format}
+use std::fmt::{format, Display, Formatter};
 
 use ranks::Rank;
 use suits::Suit;
@@ -15,13 +15,36 @@ impl Display for Card {
 }
 
 impl Card {
-  pub fn new(card: &mut str) -> Card {
-    let c: Vec<&str> = card.split("");
+  pub fn new(abbr: &mut str) -> Card {
+    let card: Vec<&str> = abbr.split("");
 
-    Card { c[0], c[1] }
+    Card {
+      rank: card[0],
+      suit: card[1],
+    }
   }
 
   pub fn to_string(&self) -> &str {
     !format!("{} of {}" self.rank.to_string(), self.suit.to_string())
+  }
+}
+
+#[cfg(test)]
+mod card_tests {
+  use super::*;
+
+  // #[test]
+  // fn should_create_ace_of_spades() {
+  //   let card = Card::from("AS");
+
+  //   assert_eq!
+  // }
+
+  #[test]
+  #[should_panic]
+  fn should_fail_invalid_card() {
+    let card = Card::from("XX");
+
+    println!("{}", card);
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,3 @@ mod suits;
 
 pub use ranks::Rank;
 pub use suits::Suit;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
