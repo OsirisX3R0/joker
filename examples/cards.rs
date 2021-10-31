5ce00498f2e0df850d67bfe208e94b80292b4e80
Richard Harris <richardharris916@gmail.com>
2021-10-30 21:01:07 -0500

    Updated examples name

--- examples/cards.rs
+++ examples/cards.rs
@@ -1,7 +1,7 @@
 use joker::Card;
 
 fn main() {
-  let card1 = Card::from("D10").unwrap();
+  let card1 = Card::from("10D").unwrap();
   let card2 = Card::from("AS").unwrap();
   let card3 = Card::from("AZ").unwrap();
   let card4 = Card::from("XS").unwrap();
